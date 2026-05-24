1096281522944aef0a63d36b7903dc0ca422da1f
GC <geniocossa4@gmail.com>
2026-05-24 13:27:20 +0200

    Teste de URL

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      'http://localhost:5173',
+      // 'http://localhost:5173',
       'https://*.vercel.app',
     ],
     credentials: true,
